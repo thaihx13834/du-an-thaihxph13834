a56ebbf802fd5eed407640e29eed0cc16b9f2f27
anhhv1234 <93896412+thaihx13834@users.noreply.github.com>
2022-04-06 10:25:53 +0700

    fix privatẻoute

--- my-app/src/pages/PrivateRouter.js
+++ my-app/src/pages/PrivateRouter.js
@@ -8,7 +8,7 @@ const PrivateRouter = ({ children }) => {
   console.log(auth);
   if (!auth || auth.user.role == 0) {
     Modal.error({
-      title: "Tài khoản không có quyền truy cập",
+      title: "Tài khoản không được cấp quyền truy cập admin!!",
       content: "Vui lòng đăng nhập bằng tài khoản admin để vào !",
     });
     return <Navigate to="/signin" />;
